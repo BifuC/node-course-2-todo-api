56772c63bf9c587e79b4decc9045142ad2641ca4
unknown <30449576+BifuC@users.noreply.github.com>
2017-08-12 16:57:41 -0500

    Added GET todos/users.

--- server/server.js
+++ server/server.js
@@ -9,15 +9,43 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// POST - localhost:3000/todos
+// {
+// 	"text": "A test todo post from postman"
+// }
 app.post('/todos', (req, res) => {
     var todo = new Todo({text: req.body.text, completed: req.body.completed})
-    todo.save().then((doc) => {
-        res.status(200).send(doc);
+    todo.save().then((todo) => {
+        res.status(200).send(todo);
     }, (e) => {
         res.status(400).send(e);
     });
     // console.log(req.body);
-})
+});
+
+// GET - localhost:3000/todos
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        //res.status(200).send('Made it here');
+        res.send({
+            todos: todos //or just todos in ES6
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// GET - localhost:3000/users
+app.get('/users', (req, res) => {
+    User.find().then((users) => {
+        //res.status(200).send('Made it here');
+        res.send({
+            users: users //or just todos in ES6
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
