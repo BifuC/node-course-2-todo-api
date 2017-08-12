0e094cbbb0861e01728660c5725316d318393ef4
unknown <30449576+BifuC@users.noreply.github.com>
2017-08-12 16:58:35 -0500

    Added useMongoClient option to prevent deprecation for 'open' method.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 // Use promises instead of callbacks (default).
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient:true});
 
 module.exports = {mongoose};
