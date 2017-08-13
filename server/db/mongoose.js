b65ef0bf162b44377cc2ca8e174e635a0dc0cd3e
unknown <30449576+BifuC@users.noreply.github.com>
2017-08-12 22:29:26 -0500

    Added heroku and mongolab support.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 
 // Use promises instead of callbacks (default).
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient:true});
+
+// Note that you can view all the environment variables from CMD, using "set | more".
+// mongoose.connect('URI here', {useMongoClient:true});
+mongoose.connect((process.env.MONGODB_URI) ? (process.env.MONGODB_URI) : 'mongodb://localhost:27017/TodoApp', {useMongoClient:true});
+
+// if (process.env.MONGODB_URI) {
+//     // Connect using "mongolab" cloud uri.
+// } else {
+//     mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient:true});
+// }
 
 module.exports = {mongoose};
